fa1e92dda6b60eff3b7637b2ac4cd7518c30ff73
Lucas Cunha <luccmir@gmail.com>
2017-04-29 00:26:26 -0300

    Add tests for charity controller filter

--- src/backend/doeaqui/spec/controllers/charities_controller_spec.rb
+++ src/backend/doeaqui/spec/controllers/charities_controller_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 RSpec.describe CharitiesController, :type => :controller do
   describe "GET #index" do
     let!(:charity) { FactoryGirl.create(:charity) }
+    let(:other_location) { FactoryGirl.create(:location, state: "SP", city: "SP") }
+    let!(:other_charity) { FactoryGirl.create(:charity, location: other_location) }
 
     it "responds successfully with an HTTP 200 status code" do
       get :index
@@ -15,17 +17,31 @@ RSpec.describe CharitiesController, :type => :controller do
       expect(response.body).not_to be_empty
       parsed_body = JSON.parse(response.body)
       expect(parsed_body).to include("charities")
-      expect(parsed_body["charities"].length).to be(1)
+      expect(parsed_body["charities"].length).to be(2)
       expect(parsed_body["charities"].first).to include("id")
     end
 
     it "should execute call method from serializer" do
       serializer = double(:serializer)
-      allow(serializer).to receive(:call) { 1 }
+      allow(serializer).to receive(:call)
       allow(subject).to receive(:serializer) { serializer }
 
       expect(serializer).to receive(:call)
       get :index
+    end
+
+    xit "should filter by state" do
+      get :index, params: { state: "SP" }
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body).to include("charities")
+      expect(parsed_body["charities"].length).to be(1)
+    end
+
+    xit "should filter by city" do
+      get :index, params: { city: "SP" }
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body).to include("charities")
+      expect(parsed_body["charities"].length).to be(1)
     end
   end
 end
