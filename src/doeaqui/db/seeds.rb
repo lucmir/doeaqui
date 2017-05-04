df908cc66bd7c631d65de19b39ed004d269f5d6a
Lucas Cunha <luccmir@gmail.com>
2017-05-03 23:55:38 -0300

    Remove views

--- src/doeaqui/db/seeds.rb
+++ src/doeaqui/db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+locations = Location.create([
+  {
+    state: "Minas Gerais",
+    city: "Belo Horizonte",
+    district: "Pampulha",
+    street: "Mystreet",
+    number: "123",
+    complement: "1"
+  },
+  {
+    state: "Minas Gerais",
+    city: "Sete Lagoas",
+    district: "Mangabeiras",
+    street: "Mystreet",
+    number: "321",
+    complement: nil
+  },
+])
+
+Charity.create([
+  {
+    name: "Foundation",
+    description: "A foundation",
+    location: locations.first
+  },
+  {
+    name: "The Charity",
+    description: "a charity",
+    location: locations.last
+  }
+])
