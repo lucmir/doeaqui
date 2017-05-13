1a6d3a5fae9c7a806c9afe444b5766bc3b02c4dc
Lucas Cunha <luccmir@gmail.com>
2017-05-13 15:28:21 -0300

    Use browser router

--- src/doeaqui-app/src/routes.js
+++ src/doeaqui-app/src/routes.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { HashRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import App from "./components/App";
 import NotFound from './components/NotFound';
 
 const Routes = (props) => (
-  <HashRouter>
+  <BrowserRouter>
     <Switch>
       <Route exact path='/' component={App}/>
       <Route path='*' component={NotFound}/>
     </Switch>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 export default Routes;
